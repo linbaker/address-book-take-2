62318045c0b7f8b70fbd0c5c8e24ff03b3f4a0c5
Marc Davies <marcdaviesriot@gmail.com>
2019-03-29 10:12:04 -0700

    change script to track values 1, 2, or 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,15 @@
 $(function() {
   $("#survey").submit(function(event){
-		var city = $("input:radio[name=city]:checked").val();
-    var companysize = $("input:radio[name=companysize]:checked").val();
-		var team = $("input:radio[name=team]:checked").val();
-    var project = $("input:radio[name=project]:checked").val();
-    var topreason = $("input:radio[name=topreason]:checked").val();
-    // console.log(city);
-    // console.log(companysize);
-    // console.log(team);
-    // console.log(project);
-    // console.log(topreason);
-
+		var city = parseInt($("input:radio[name=city]:checked").val());
+    var companysize = parseInt($("input:radio[name=companysize]:checked").val());
+		var team = parseInt($("input:radio[name=team]:checked").val());
+    var project = parseInt($("input:radio[name=project]:checked").val());
+    var topreason = parseInt($("input:radio[name=topreason]:checked").val());
+    console.log(city);
+    console.log(companysize);
+    console.log(team);
+    console.log(project);
+    console.log(topreason);
 
     // if (side1 + side2 >= side3 && side1 + side3 >= side2 && side2 + side3 >= side1 && side1 !== 0 && side2 !== 0 && side3 !== 0) {
     //   if (side1 === side2 && side1 === side3 && side2 === side3) {
