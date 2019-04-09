525b88900a16f5fa0dda644f15fdf549a419e628
Lindsey Baker and Marc Davies <lindsey@>
2019-04-09 11:35:19 -0700

    Add object for email addresses

--- js/scripts.js
+++ js/scripts.js
@@ -40,15 +40,19 @@ AddressBook.prototype.deleteContact = function(id) {
 
 // Business Logic for Contacts -------------------------------------------------
 
-function Contact(firstName, lastName, phoneNumber, emailPersonalAddress, emailWorkAddress, physicalHomeAddress, physicalWorkAddress) {
+function Contact(firstName, lastName, phoneNumber, inputtedEmailPersonalAddress, inputtedEmailWorkAddress, physicalAddress) {
   this.firstName = firstName,
   this.lastName = lastName,
   this.phoneNumber = phoneNumber,
-  this.emailPersonalAddress = emailPersonalAddress,
-  this.emailWorkAddress = emailWorkAddress,
-  this.physicalHomeAddress = physicalHomeAddress,
-  this.physicalWorkAddress = physicalWorkAddress
+  this.email =  new EmailAddresses(inputtedEmailPersonalAddress, inputtedEmailWorkAddress);
+  this.physicalAddress = physicalAddress
+  // this.physicalWorkAddress = physicalWorkAddress
   }
+
+function EmailAddresses(emailPersonalAddress, emailWorkAddress) {
+  this.emailPersonalAddress = emailPersonalAddress,
+  this.emailWorkAddress = emailWorkAddress
+}
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
@@ -73,10 +77,10 @@ function showContact(contactId) {
   $(".first-name").html(contact.firstName);
   $(".last-name").html(contact.lastName);
   $(".phone-number").html(contact.phoneNumber);
-  $(".email-personal-address").html(contact.emailPersonalAddress);
-  $(".email-work-address").html(contact.emailWorkAddress);
-  $(".physical-home-address").html(contact.physicalHomeAddress);
-  $(".physical-work-address").html(contact.physicalWorkAddress);
+  $(".email-personal-address").html(contact.email.emailPersonalAddress);
+  $(".email-work-address").html(contact.email.emailWorkAddress);
+  $(".physical-address").html(contact.physicalAddress);
+  // $(".physical-work-address").html(contact.physicalWorkAddress);
   var buttons = $("#buttons");
   buttons.empty();
   buttons.append("<button class='deleteButton' id=" +  + contact.id + ">Delete</button>");
@@ -102,17 +106,19 @@ $(document).ready(function() {
     var inputtedPhoneNumber = $("input#new-phone-number").val();
     var inputtedEmailPersonalAddress = $("input#new-email-personal-address").val();
     var inputtedEmailWorkAddress = $("input#new-email-work-address").val();
-    var inputtedPhysicalHomeAddress = $("input#new-physical-home-address").val();
-    var inputtedPhysicalWorkAddress = $("input#new-physical-work-address").val();
+    var inputtedPhysicalAddress = $("input#new-physical-address").val();
+    // var inputtedPhysicalWorkAddress = $("input#new-physical-work-address").val();
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
     $("input#new-phone-number").val("");
     $("input#new-email-personal-address").val("");
     $("input#new-email-work-address").val("");
-    $("input#new-physical-home-address").val("");
-    $("input#new-physical-work-address").val("");
-    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmailPersonalAddress, inputtedEmailWorkAddress, inputtedPhysicalHomeAddress, inputtedPhysicalWorkAddress );
+    $("input#new-physical-address").val("");
+    // $("input#new-physical-work-address").val("");
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmailPersonalAddress, inputtedEmailWorkAddress, inputtedPhysicalAddress);
     addressBook.addContact(newContact);
+    console.log("End ot UI contacts", newContact);
     displayContactDetails(addressBook);
   })
 })
